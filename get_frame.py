bfcefb690e34f4fd64fa91d03325999dd52582d3
khai9xht <khaihoang9x@gmail.com>
2021-03-30 13:43:08 +0700

    Fix bug get frame

--- get_frame.py
+++ get_frame.py
@@ -1,7 +1,10 @@
-from utils import get_img_from_url, process_json_message
+from paho.mqtt.client import topic_matches_sub
+from utils import get_img_from_url, process_json_message, convert_timestamp
 import cv2
 import os
+from datetime import datetime
 from glob import glob
+from pathlib import Path
 #Below code will capture the video frames and will sve it a folder (in current working directory)
 
 def get_video_start_time(username, password, ip_address, port, channel, stream, 
@@ -54,22 +57,25 @@ def read_log_file(log_path):
 
     return line_objects, [yaw_offset, pitch_offset, row_offset]
          
-def get_frame_from_log(username, password, ip_address, port, channel, stream, 
-                        starting_year, starting_month, starting_day, 
-                        starting_hour, starting_minute, starting_second, timestamp,
+def get_frame_from_log(username, password, ip_address, port, channel, stream, timestamp,
                         path_save_frame, log_path):
     """
     input:
     output:
     """
-    timestamp_frame_start = timestamp - 2
-    starting_second = int(starting_second) - 2
+    # read all line and sort by time
+    line_object, _ = read_log_file(log_path)
+    timestamp_frame_start = line_object[0]['time']
+    
+    datetime_objects = convert_timestamp(int((timestamp_frame_start)/1000)).split("_")
+    starting_year, starting_month, starting_day = datetime_objects[0], datetime_objects[1], datetime_objects[2]
+    starting_hour, starting_minute, starting_second = datetime_objects[3], datetime_objects[4], datetime_objects[5]
+    
+    starting_second = int(float(starting_second))
     uri = f"rtsp://{username}:{password}@{ip_address}:{port}/Streaming/tracks/{channel}{stream}?starttime={format(int(starting_year), '04d')}{format(int(starting_month), '02d')}{format(int(starting_day), '02d')}T{format(int(starting_hour) - 7, '02d')}{format(int(starting_minute), '02d')}{str(format(int(starting_second), '02d'))}z"
     # uri = "rtsp://admin:abcd1234@192.168.10.75:554/Streaming/tracks/101?starttime=20201111T111111z"
     print(uri)
-
-    # read all line and sort by time 
-    line_object, _ = read_log_file(log_path)
+    
 
     #video path
     cap = cv2.VideoCapture(uri)
@@ -86,6 +92,8 @@ def get_frame_from_log(username, password, ip_address, port, channel, stream,
         fps = cap.get(cv2.CAP_PROP_FPS)
         print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
     count_frame = 0
+
+    Path(path_save_frame).mkdir(parents=True, exist_ok=True)
     
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -94,7 +102,7 @@ def get_frame_from_log(username, password, ip_address, port, channel, stream,
         else:
             # cv2.imshow('frame', frame)
             #The received "frame" will be saved. Or you can manipulate "frame" as per your needs.
-            timestamp_frame = timestamp_frame_start + (count_frame * (1/fps))
+            timestamp_frame = timestamp_frame_start + (count_frame * (1000/fps))
             print(f"[INFO]-{timestamp_frame}-|-{line_object[0]['time']}-|-{len(line_object)}-----", end="\r") 
             
             name = f"rec_frame_{format(int(starting_year), '04d')}{format(int(starting_month), '02d')}{format(int(starting_day), '02d')}T{format(int(starting_hour), '02d')}{format(int(starting_minute), '02d')}{starting_second + count_frame * (1.0/fps)}_{timestamp_frame}.jpg"
@@ -102,7 +110,8 @@ def get_frame_from_log(username, password, ip_address, port, channel, stream,
             # 1609901392.553
             # 1609913255.610
             # 1609901673
-            if abs(timestamp_frame * 1000 - line_object[0]['time']) < (1000 / (fps)):
+            # print(timestamp_frame, "-", line_object[0]['time'])
+            if abs(timestamp_frame - line_object[0]['time']) < (1000 / (fps)):
                 cv2.imwrite(os.path.join(path_save_frame,name), frame)
                 del line_object[0]
                 if len(line_object) == 0:
@@ -132,14 +141,13 @@ def get_frame_from_all_log(username, password, ip_address, port, channel, stream
         second = list_info[5].split(".")[0]
         timestamp = float(list_info[6][:-4])
         
-        get_frame_from_log(username, password, ip_address, port, channel, stream, 
-                            year, month, day, hour, minute, second, timestamp,
+        get_frame_from_log(username, password, ip_address, port, channel, stream, timestamp,
                             path_save_frame, log_path)
 
 if __name__ == "__main__":
     username = "admin"
     password = "abcd1234"
-    ip_address = "192.168.10.75"
+    ip_address = "192.168.15.11"
     port = "554"
     channel = "1"
     stream = "01"
@@ -149,7 +157,7 @@ if __name__ == "__main__":
     hour = "11"
     minute = "11"
     second = "11"
-    path_save_frame = "imgs"
+    path_save_frame = "/home/linhnv/projects/label-studio/imgs"
     # get_video_start_time(username, password, ip_address, port, channel, stream, year, month, day, hour, minute, second, path_save_frame)
     log_dir = "data/logs"
     frames_dir = "data/frames"
